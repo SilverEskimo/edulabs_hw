81baf432d69913970a7e14a7c0e161a48b0f7305
SilverEskimo <slavasereb@gmail.com>
2023-01-17 21:09:00 +0200

    uplooad example for Daniel

---
+++ lesson9/example/main.py
@@ -0,0 +1,30 @@
+from example.BankAccount import *
+
+if __name__ == '__main__':
+    acc_holder = {
+            "passport_num": 123123,
+            "name": "Daniel",
+            "address": "Rishon",
+            "phone": 1023123
+    }
+    daniel_ba = BankAccount(321321, acc_holder, 0, 0, 100)
+
+    if daniel_ba.deposit(200, "nis"):
+        print("Success deposit")
+    else:
+        print("Failed Deposit")
+
+    # if daniel_ba.withdraw(500, "nis"):
+    #     print("Success withdraw")
+    # else:
+    #     print("Failed withdraw")
+
+    if daniel_ba.convert(100, "nis"):
+        print("Convert done")
+    else:
+        print("Convert failed")
+
+    print(daniel_ba.get_current_balance("nis"))
+    print(daniel_ba.get_current_balance("usd"))
+    print(daniel_ba.get_transactions_per_date("2023-01-17"))
+
